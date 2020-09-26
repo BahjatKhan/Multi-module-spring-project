6f41a543834383693c2f6d51450a95ebd9df8ea1
Bahjat Khan <bahjat_khan2002@yahoo.com>
2020-09-26 02:33:05 -0400

    no message

---
+++ core/src/main/java/com/picante/Game.java
@@ -0,0 +1,15 @@
+package com.picante;
+
+public interface Game {
+
+    int getNumber();
+
+    int getGuess();
+
+    int setGuess(int guess);
+
+    int getSmallest();
+
+
+
+}
---
+++ core/src/main/java/com/picante/NumberGenerator.java
@@ -0,0 +1,7 @@
+package com.picante;
+
+public interface NumberGenerator {
+    int next();
+    int getMaxNumber();
+
+}
